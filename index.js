cc18ccae56df93be9959da3a2a0a451d66ff45ad
igao94 <igao998@gmail.com>
2024-02-19 11:05:58 +0100

    renamed images folder

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const rollDice = () => {
   for (let i = 0; i < numOfDice; i++) {
     const value = Math.floor(Math.random() * 6) + 1;
     values.push(value);
-    images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
+    images.push(`<img src="public/${value}.png" alt="Dice ${value}">`);
   }
 
   diceResult.textContent = `Dice: ${values.join(", ")}`;
